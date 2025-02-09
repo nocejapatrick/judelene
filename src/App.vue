c1391acdb543ff8a67a5539c33cc2339882320e8
nocejapatrick <nocejapatrick@gmail.com>
2025-02-09 15:28:20 +0800

    For Valentines

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <h1 class="text-3xl darumadrop-one-regular text-center mt-3">
+      Pat and Judy
+    </h1>
+    <div class="flex justify-center mt-10">
+      <img src="./assets/jude1.jpg" class="w-sm shadow-md" alt="">
+    </div>
+  
+  <!-- <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style scoped>
+
+</style>
